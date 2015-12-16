c479361c0bc1302c8e526448ffb7dae10659c051
Toni <toniconmac@gmail.com>
2015-12-16 12:47:34 +0100

    Add initial project files

---
+++ openweather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  openweather
+//
+//  Created by Toni on 16/12/15.
+//  Copyright © 2015 Toni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ openweather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  openweather
+//
+//  Created by Toni on 16/12/15.
+//  Copyright © 2015 Toni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
